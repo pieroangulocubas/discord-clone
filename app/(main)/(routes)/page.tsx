c9c363e48f3892b2a99a2910e28f0441f1637726
pieroangulocubas <pieroangulocubas.17@gmail.com>
2023-11-20 21:25:50 +0100

    first commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/ui/mode-toggle"
+import { UserButton } from "@clerk/nextjs"
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton 
+        afterSignOutUrl="/"
+      />
+      <ModeToggle />
+    </div>
+  )
+}
